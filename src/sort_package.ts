1aec3d44be5ed223b957a382e99cb757c0f7474d
rb74 <rbgenius74@gmail.com>
2025-11-23 12:58:30 -0600

    C:/Program Files/Git/src/sort_package.ts created

---
+++ src/sort_package.ts
@@ -0,0 +1,9 @@
+export function sort(width: number, height: number, length: number, mass: number): string {
+    const volume = width * height * length;
+    const isBulky = volume >= 1000000 || width >= 150 || height >=150 || length >=150;
+    const isHeavy = mass >= 20;
+
+    if (isBulky && isHeavy) return "REJECTED";
+    if (isBulky || isHeavy) return "SPECIAL";
+    return "STANDARD";
+}
